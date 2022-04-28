cc7437838a584863f9f6a1eed655460396f3ce8d
LuizHenrique <luizhenrique@pop-os.localdomain>
2022-04-28 14:49:59 -0300

    add breakpoints

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
 let display = document.getElementById("display")
 
 function clean() {
-     display.innerHTML = " "
-    
+    display.innerHTML = " "
 }
 
-//função insert > ao clicar no item(numero, operador ou ponto) insere no display
-//é criado uma variavel chamado numero que posteriormente é colocado ao lado do que valor já existente
 function insert(num) {
     let numero = display.innerHTML
     display.innerHTML = numero + num
 }
 
-//Função back > volta uma casa apagando o valor anterior
 function back() {
     var resultado = display.innerHTML
     display.innerHTML = resultado.substring(0, resultado.length -1)
 }
 
-//Realiza o calculo 
 function calc() {
-    var calculo = display.innerHTML
+    var calculo = display.innerHTML 
     if (calculo) {
         display.innerHTML = eval(calculo)
     } else {
